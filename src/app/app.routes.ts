ce4f6ee4ae958f1f6546977b1a8f8f75871346af
Benjamin Skeen <benjamin.skeen0@gmail.com>
2023-06-14 21:09:20 -0600

    I added a game component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { GameLayoutComponent } from './game/game-layout/game-layout.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'game',
+  },
+  {
+    path: 'game',
+    component: GameLayoutComponent,
+  },
+];
---
+++ src/app/game/game-layout/game-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ttt-game-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-layout.component.html',
+  styleUrls: ['./game-layout.component.scss'],
+})
+export class GameLayoutComponent {}
